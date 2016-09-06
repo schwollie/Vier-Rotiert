873f93d13087f585551d2f618b748220d1c19c25
Andy <Andreas@familiechristiansen.de>
2016-09-06 10:25:41 +0200

    show,setBoard

--- app/src/main/java/com/example/lars/vierrotiert/MainActivity.java
+++ app/src/main/java/com/example/lars/vierrotiert/MainActivity.java
@@ -3,24 +3,72 @@ package com.example.lars.vierrotiert;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.ImageButton;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private int size = 5;
+    private Board board = new Board(size);
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.field);
 
+        setSize();
 
+        board = board.set(0, Board.Field.Black);
+        board = board.set(1, Board.Field.White);
+        board = board.set(1, Board.Field.Black);
+        board = board.set(0, Board.Field.White);
+        board = board.set(2, Board.Field.Black);
+        board = board.set(0, Board.Field.White);
+        board = board.set(3, Board.Field.Black);
 
-
+        showBoard();
     }
 
+    void setSize() {
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.field);
+        gridLayout.setColumnCount(size);
+        gridLayout.setRowCount(size);
+        gridLayout.removeAllViews();
 
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                View childView = new ImageView(this);
 
+                GridLayout.Spec rowspan = GridLayout.spec(row, 1);
+                GridLayout.Spec colspan = GridLayout.spec(col, 1);
+                GridLayout.LayoutParams gridLayoutParam = new GridLayout.LayoutParams(rowspan, colspan);
+                gridLayoutParam.width = 64;
+                gridLayoutParam.height = 64;
+
+                gridLayout.addView(childView, gridLayoutParam);
+            }
+        }
+    }
+
+    void showBoard() {
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.field);
+        int index = 0;
+        for (int row = 0; row < size; row++) {
+            for (int col = 0; col < size; col++, index++) {
+                ImageView imageView = (ImageView) gridLayout.getChildAt(index);
+                switch (board.get(row, col)) {
+                    case Black:
+                        imageView.setImageDrawable(getDrawable(R.drawable.ic_circle_black_24dp));
+                        break;
+                    case White:
+                        imageView.setImageDrawable(getDrawable(R.drawable.ic_circle_white_24dp));
+                        break;
+                }
+            }
+        }
+    }
 }
 
 
