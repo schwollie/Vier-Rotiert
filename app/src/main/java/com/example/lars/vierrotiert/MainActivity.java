71d1c2cc4355a6e07fb2ec6148e9af330f696571
Andy <Andreas@familiechristiansen.de>
2016-09-12 18:22:38 +0200

    turnable

--- app/src/main/java/com/example/lars/vierrotiert/MainActivity.java
+++ app/src/main/java/com/example/lars/vierrotiert/MainActivity.java
@@ -3,14 +3,15 @@ package com.example.lars.vierrotiert;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private int size = 5;
     private Board board = new Board(size);
@@ -32,49 +33,32 @@ public class MainActivity extends AppCompatActivity {
         board = board.set(3, Board.Field.Black);
 
         showBoard();
+
+        ImageButton rotateLeftButton = (ImageButton) findViewById(R.id.rotate_left);
+        rotateLeftButton.setOnClickListener(this);
+
+
+        ImageButton rotateRightButton = (ImageButton) findViewById(R.id.rotate_right);
+        rotateRightButton.setOnClickListener(this);
     }
 
     void setSize() {
-
-
         GridLayout gridLayout = (GridLayout) findViewById(R.id.field);
-
-        RelativeLayout right = (RelativeLayout) findViewById(R.id.rechts);
-
-        RelativeLayout left = (RelativeLayout) findViewById(R.id.links);
-
 
         gridLayout.setColumnCount(size);
         gridLayout.setRowCount(size);
         gridLayout.removeAllViews();
-
-
-        Display display = getWindowManager().getDefaultDisplay();
-        int stageWidth = display.getWidth();
-        int stageheight = display.getHeight();
-
-
-
-
+        gridLayout.setPadding(64, 64, 64, 64);
+        gridLayout.setBackgroundColor(Color.BLUE);
 
         for (int row = 0; row < size; row++) {
             for (int col = 0; col < size; col++) {
                 View childView = new ImageView(this);
 
-                GridLayout.Spec rowspan = GridLayout.spec(row, 1);
-                GridLayout.Spec colspan = GridLayout.spec(col, 1);
+                GridLayout.Spec rowspan = GridLayout.spec(row, 1, 1);
+                GridLayout.Spec colspan = GridLayout.spec(col, 1, 1);
                 GridLayout.LayoutParams gridLayoutParam = new GridLayout.LayoutParams(rowspan, colspan);
-
-
-                int height = (stageheight / 7) / 2;
-
-
-                gridLayoutParam.width = stageWidth / 7;
-                gridLayoutParam.height = gridLayoutParam.width;
-                gridLayout.setBackgroundColor(Color.BLUE);
-                gridLayout.setPadding(gridLayoutParam.width, height, 0, 0);
-
-
+                gridLayoutParam.setMargins(8, 8, 8, 8);
 
                 gridLayout.addView(childView, gridLayoutParam);
             }
@@ -105,6 +89,54 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.rotate_left) {
+            GridLayout gridLayout = (GridLayout) findViewById(R.id.field);
+
+
+            Animation a = AnimationUtils.loadAnimation(this, R.anim.rotate_left);
+            Animation b = AnimationUtils.loadAnimation(this, R.anim.rotate_left_90);
+
+            ImageButton btn2 = (ImageButton) findViewById(R.id.rotate_left);
+            a.setAnimationListener(new SimpleAnimationListener() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                    board = board.rotateLeft();
+                    showBoard();
+
+                }
+            });
+            btn2.startAnimation(a);
+            gridLayout.startAnimation(b);
+
+        } else if (view.getId() == R.id.rotate_right) {
+            GridLayout gridLayout = (GridLayout) findViewById(R.id.field);
+
+
+            Animation a = AnimationUtils.loadAnimation(this, R.anim.rotate_right);
+            Animation b = AnimationUtils.loadAnimation(this, R.anim.rotate_right_90);
+
+
+            ImageButton btn2 = (ImageButton) findViewById(R.id.rotate_right);
+            a.setAnimationListener(new SimpleAnimationListener() {
+                @Override
+                public void onAnimationEnd(Animation animation) {
+
+                    board = board.rotateLeft();
+                    showBoard();
+
+                }
+            });
+            btn2.startAnimation(a);
+            gridLayout.startAnimation(b);
+
+        }
+
+
     }
 }
 
